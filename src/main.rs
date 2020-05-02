2178f952d2fad1369cf2962812212acfe88c39f2
Alan Rodriguez <alan.9489@gmail.com>
2020-05-02 17:36:04 -0500

    Se agrego el programa para togglear un led

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,25 @@ use stm32l4::stm32l4x6;
 #[entry]
 fn main() -> ! {
     let peripherals = stm32l4x6::Peripherals::take().unwrap();
+    let gpioa = &peripherals.GPIOA;
+    let rcc = &peripherals.RCC;
     hprintln!("Perifericos creados").unwrap();
+    
+    // Se debe habilitar el reloj para el GPIOA
+    rcc.ahb2enr.write(|w| w.gpioaen().set_bit());
+
+    // Se configura el periferico para funcionar como salida
+    gpioa.moder.write(|w| w.moder5().bits(0b01));
+    gpioa.otyper.write(|w| w.ot5().bit(false));
+    gpioa.ospeedr.write(|w| w.ospeedr5().bits(0b00));
+    gpioa.pupdr.write(|w| unsafe{w.pupdr5().bits(0b00)});
+    hprintln!("Se configuro el pin 5 del puerto A").unwrap();
+    
+    
     loop {
-        // your code goes here
+        gpioa.bsrr.write(|w| w.bs5().set_bit());
+        cortex_m::asm::delay(2000000);
+        gpioa.bsrr.write(|w| w.br5().set_bit());
+        cortex_m::asm::delay(2000000);
     }
 }
