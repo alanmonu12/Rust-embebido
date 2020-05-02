594aeacdb995a3167606a7209139256b0588df9e
Alan Rodriguez <alan.9489@gmail.com>
2020-05-02 15:27:15 -0500

    Se agrego un codigo base para poder compilar el proyecto

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+// pick a panicking behavior
+extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+extern crate cortex_m_rt;
+extern crate cortex_m;
+extern crate stm32l4;
+
+use cortex_m::asm;
+use cortex_m_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+
+    loop {
+        // your code goes here
+    }
 }
